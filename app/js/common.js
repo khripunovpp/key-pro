77fd49fa192bbc803757771272c2e52f08ae354d
Павел Хрипунов <khripunovpp@gmail.com>
2020-02-13 14:55:19 +0300

    nav and hero mobile done

--- app/js/common.js
+++ app/js/common.js
@@ -1,17 +1,23 @@
 var Util = {
-    randomInteger: function(min, max) {
-        var rand = min + Math.random() * (max - min)
-        rand = Math.round(rand);
-        return rand;
-    },
-    scrollToEl: function(el, offset) {
-        $("html,body").animate({ scrollTop: el.offset().top + (offset || 0) }, 500);
-    },
-    trimString: function(string) {
-        return string.split(' ').join('');
-    }
-}
+  randomInteger: function(min, max) {
+    var rand = min + Math.random() * (max - min);
+    rand = Math.round(rand);
+    return rand;
+  },
+  scrollToEl: function(el, offset) {
+    $("html,body").animate({ scrollTop: el.offset().top + (offset || 0) }, 500);
+  },
+  trimString: function(string) {
+    return string.split(" ").join("");
+  }
+};
 
 $(function() {
-
+  var nav = $(".nav"),
+    navBtn = $(".nav__btn");
+  navBtn.on("click", function(e) {
+    e.preventDefault();
+    navBtn.toggleClass("opened");
+    nav.toggleClass("opened");
+  });
 });
