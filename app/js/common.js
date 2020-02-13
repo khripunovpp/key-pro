515d4d0beb0487e7faf4b99f09a0a1191632d40b
Павел Хрипунов <khripunovpp@gmail.com>
2020-02-13 15:52:20 +0300

    added navs scripts

--- app/js/common.js
+++ app/js/common.js
@@ -12,6 +12,50 @@ var Util = {
   }
 };
 
+$.fn.isOnScreen = function(shift) {
+  if (!shift) {
+    shift = 0;
+  }
+  var viewport = {};
+  viewport.top = $(window).scrollTop();
+  viewport.bottom = viewport.top + $(window).height();
+  var bounds = {};
+  bounds.top = this.offset().top + shift;
+  bounds.bottom = bounds.top + this.outerHeight() - shift;
+  return bounds.top <= viewport.bottom && bounds.bottom >= viewport.top;
+};
+
+var shift = 90;
+
+var scrollDetection = function() {
+  $("section").each(function() {
+    var that = this;
+
+    if ($(that).isOnScreen(shift)) {
+      hightlight($(that).attr("id"));
+    }
+  });
+
+  var scrollTop = $(document).scrollTop();
+
+  if (scrollTop > 5) {
+  } else {
+  }
+};
+
+var navItems = $(".nav li");
+
+var hightlight = function(id) {
+  if (!id) return;
+  console.log(id);
+  navItems.find("a").removeClass("active");
+  navItems
+    .find('a[data-href="' + id + '"]')
+    .addClass("active")
+    .closest("li")
+    .addClass("active");
+};
+
 $(function() {
   var nav = $(".nav"),
     navBtn = $(".nav__btn");
@@ -19,5 +63,15 @@ $(function() {
     e.preventDefault();
     navBtn.toggleClass("opened");
     nav.toggleClass("opened");
+  });
+
+  scrollDetection();
+  $(document).on("scroll", scrollDetection);
+
+  $(".nav a, .scroll").on("click", function(e) {
+    e.preventDefault();
+    var id = $(this).attr("data-href");
+    var section = $("#" + id).offset().top;
+    $("html, body").animate({ scrollTop: section }, 666);
   });
 });
